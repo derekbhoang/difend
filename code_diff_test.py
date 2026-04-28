e52e7dc2afaeef04e55d1f3d45ba8c53091e4fe7
HoangBuiVietDuc28102006 <derek.b.hoang@gmail.com>
2026-04-28 22:32:43 +1000

    Add a sample code file for code diff check

---
+++ code_diff_test.py
@@ -0,0 +1,12 @@
+def greet(name):
+    return f"Hello, {name}!"
+
+
+def main():
+    user_name = "Difend"
+    message = greet(user_name)
+    print(message)
+
+
+if __name__ == "__main__":
+    main()
